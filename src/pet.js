1051bfd72bb2fd1e15b7638696abf8eeb878b7c5
Armghan <armghanz@gmail.com>
2023-06-11 12:43:23 +0100

    Review Comments embodied but getting 1 failed test.

--- src/pet.js
+++ src/pet.js
@@ -1,31 +1,33 @@
-const minFitness = 0;
-const minAcceptableFitness = 3;
-const maxFitness = 10;
-const minHunger = 0;
-const minAcceptableHunger = 5;
-const maxHunger = 10;
-const maxAge = 30;
+const MIN_FITNESS = 0;
+const MIN_ACCEPTABLE_FITNESS = 3;
+const MAX_FITNESS = 10;
+const MIN_HUNGER = 0;
+const MIN_ACCEPTABLE_HUNGER = 5;
+const MAX_HUNGER = 10;
+const MAX_AGE = 30;
+const DEAD_PET_ERROR = "(*_*) Your pet has died.";
 
 function Pet(name) {
   this.name = name;
   this.age = 0;
-  this.hunger = minHunger;
-  this.fitness = maxFitness;
+  this.hunger = MIN_HUNGER;
+  this.fitness = MAX_FITNESS;
+  this.children = [];
 }
 
 Pet.prototype = {
   get isAlive() {
     return (
-      this.age < maxAge &&
-      this.hunger < maxHunger &&
-      this.fitness > minFitness
+      this.age < MAX_AGE &&
+      this.hunger < MAX_HUNGER &&
+      this.fitness > MIN_FITNESS
     );
   },
 };
 
 Pet.prototype.growUp = function () {
   if (!this.isAlive) {
-    throw new Error("(*_*) Your pet has died.");
+    throw new Error(DEAD_PET_ERROR);
   }
   this.age += 1;
   this.hunger += 5;
@@ -34,42 +36,34 @@ Pet.prototype.growUp = function () {
 
 Pet.prototype.walk = function () {
   if (!this.isAlive) {
-    throw new Error("(*_*) Your pet has died.");
+    throw new Error(DEAD_PET_ERROR);
   }
-  this.fitness = Math.min(this.fitness + 4, maxFitness);
+  this.fitness = Math.min(this.fitness + 4, MAX_FITNESS);
 };
 
 Pet.prototype.feed = function () {
   if (!this.isAlive) {
-    throw new Error("(*_*) Your pet has died.");
+    throw new Error(DEAD_PET_ERROR);
   }
-  this.hunger = Math.max(this.hunger - 3, minHunger);
+  this.hunger = Math.max(this.hunger - 3, MIN_HUNGER);
 };
 
 Pet.prototype.checkUp = function () {
   if (!this.isAlive) {
-    throw new Error("(*_*) Your pet has died.");
+    throw new Error(DEAD_PET_ERROR);
   }
-  if (
-    this.fitness <= minAcceptableFitness &&
-    this.hunger >= minAcceptableHunger
-  ) {
-    return "I am hungry AND I need a walk!";
-  }
-  if (this.fitness <= minAcceptableFitness) {
-    return "I need a walk!";
-  }
-  if (this.hunger >= minAcceptableHunger) {
-    return "I am hungry!";
-  }
-  return "I feel great!";
+  const needsWalk = this.fitness <= MIN_ACCEPTABLE_FITNESS;
+  const isHungry = this.hunger >= MIN_ACCEPTABLE_HUNGER;
+
+  if (needsWalk && isHungry) return "I am hungry AND I need a walk!"
+  if (needsWalk) return "I need a walk!";
+  if (isHungry) return "I am hungry!";
 };
 
 Pet.prototype.haveBaby = function (childName) {
   if (!this.isAlive) {
-    throw new Error("(*_*) Your pet has died.");
+    throw new Error(DEAD_PET_ERROR);
   }
-  this.children = this.children || [];
   this.children.push(new Pet(childName));
 };
 
